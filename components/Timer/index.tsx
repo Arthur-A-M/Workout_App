074aa8b1643f875e11eada36447cbdb9499d804a
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-17 19:31:09 -0300

    Display time passed when Timer is running
    Timer can now display the time passed in seconds for all the time it was left running.

--- components/Timer/index.tsx
+++ components/Timer/index.tsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Pressable, Text, View } from "react-native";
 
 export default function Timer() {
   const [running, setRunning] = useState(false);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    if (running) {
+      const intervalId = setInterval(() => setTime((prevTime) => prevTime + 1), 1000);
+      return () => clearInterval(intervalId);
+    }
+  }, [running]);
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "white" }}>
@@ -11,6 +19,7 @@ export default function Timer() {
         onPress={() => setRunning(!running)}
       >
         <Text>Timer</Text>
+        {running && <Text>{time} seconds</Text>}
       </Pressable>
     </View>
   );
