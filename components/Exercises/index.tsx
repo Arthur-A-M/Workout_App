39e7ad4376c29dc7b596cf2e709971445277986d
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-17 18:07:21 -0300

    Parse exercise data from variables
    Data about the number of repetitions, series, and weight are no longer hard coded on the component, but rather on the parsed objects in the array.

--- components/Exercises/index.tsx
+++ components/Exercises/index.tsx
@@ -6,31 +6,57 @@ import { styles } from './styles';
 type ExerciseItem = {
     key: string;
     name: string;
+    series: number;
+    repetitions: number;
+    weight: number;
 };
 
 const ExerciseList: ExerciseItem[] = [
-    { key: "1", name: "Exercicio 1" },
-    { key: "2", name: "Exercicio 2" },
-    { key: "3", name: "Exercicio 3" },
+    {
+        key: "1",
+        name: "Exercicio 1",
+        series: 3,
+        repetitions: 8,
+        weight: 20
+    },
+    {
+        key: "2",
+        name: "Exercicio 2",
+        series: 3,
+        repetitions: 8,
+        weight: 20
+    }, {
+        key: "3",
+        name: "Exercicio 3",
+        series: 3,
+        repetitions: 8,
+        weight: 20
+    },
 ];
 
 export default function Exercises() {
-    function Exercise({ key, name }: ExerciseItem) {
+    function Exercise({
+        key,
+        name,
+        series,
+        repetitions,
+        weight
+    }: ExerciseItem) {
         return (
             <View style={styles.page} key={key}>
                 <View style={styles.box}>
                     <Text>{name}</Text>
                     <View style={styles.boxRow}>
                         <Text>Séries</Text>
-                        <Text>3</Text>
+                        <Text>{series}</Text>
                     </View>
                     <View style={styles.boxRow}>
                         <Text>Repetições</Text>
-                        <Text>8</Text>
+                        <Text>{repetitions}</Text>
                     </View>
                     <View style={styles.boxRow}>
                         <Text>Carga</Text>
-                        <Text>20</Text>
+                        <Text>{weight}</Text>
                     </View>
                 </View>
             </View>
@@ -41,7 +67,13 @@ export default function Exercises() {
         <View style={styles.container}>
             <PagerView style={styles.container} initialPage={0}>
                 {ExerciseList.map((item) => (
-                    <Exercise key={item.key} name={item.name} />
+                    <Exercise
+                        key={item.key}
+                        name={item.name}
+                        series={item.series}
+                        repetitions={item.repetitions}
+                        weight={item.weight}
+                    />
                 ))}
             </PagerView>
         </View>
