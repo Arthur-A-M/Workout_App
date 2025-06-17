e8c5027e62fb8160b9af7699563238832b703af1
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-17 19:25:37 -0300

    Make Timer a standalone component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,12 @@
 import Exercises from "@/components/Exercises";
-import { Text, View } from "react-native";
+import Timer from "@/components/Timer";
+import { View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, flexDirection: "column" }}>
       <Exercises />
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "white" }}>
-        <Text>Timer</Text>
-      </View>
+      <Timer />
     </View>
   );
 }
