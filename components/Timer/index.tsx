1a152a45aeb24f243a955a604a4732f528f2d406
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-17 19:29:24 -0300

    Add Pressable to Timer
    Timer now can check if the Timer if running and change it's color.

--- components/Timer/index.tsx
+++ components/Timer/index.tsx
@@ -1,9 +1,17 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Pressable, Text, View } from "react-native";
 
 export default function Timer() {
+  const [running, setRunning] = useState(false);
+
   return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "white" }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "white" }}>
+      <Pressable
+        style={{ backgroundColor: running ? "gray" : "lightgray" }}
+        onPress={() => setRunning(!running)}
+      >
         <Text>Timer</Text>
-      </View>
+      </Pressable>
+    </View>
   );
 }
