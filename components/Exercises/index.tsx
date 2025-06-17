15208f87ef9f1267d3f3cf30548b90b7429ef4e2
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-17 17:42:36 -0300

    Update index.tsx
    Each exercise is now generated by it's own function.

--- components/Exercises/index.tsx
+++ components/Exercises/index.tsx
@@ -4,15 +4,20 @@ import PagerView from 'react-native-pager-view';
 import { styles } from './styles';
 
 export default function Exercises() {
+    function Exercise({key, name}: {key: string, name: string}) {
+        return (
+            <View style={[styles.page, styles.container]} key={key}>
+                <Text>{name} </Text>
+            </View>
+        );
+    }
+
     return (
         <View style={styles.container}>
             <PagerView style={styles.container} initialPage={0}>
-                <View style={styles.page} key="1">
-                    <Text>Exercise 1</Text>
-                </View>
-                <View style={styles.page} key="2">
-                    <Text>Exercise 2</Text>
-                </View>
+                <Exercise key="1" name="Exercicio 1" />
+                <Exercise key="2" name="Exercicio 2" />
+                <Exercise key="3" name="Exercicio 3" />
             </PagerView>
         </View>
     );
