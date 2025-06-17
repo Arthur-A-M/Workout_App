dde0abe6b75ee42fb7f5822fd37c5df4062955ea
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-17 17:58:35 -0300

    Style change
    There is a, visible to the user, box containing the information about each exercise.

--- components/Exercises/index.tsx
+++ components/Exercises/index.tsx
@@ -4,32 +4,46 @@ import PagerView from 'react-native-pager-view';
 import { styles } from './styles';
 
 type ExerciseItem = {
-  key: string;
-  name: string;
+    key: string;
+    name: string;
 };
 
 const ExerciseList: ExerciseItem[] = [
-  { key: "1", name: "Exercicio 1" },
-  { key: "2", name: "Exercicio 2" },
-  { key: "3", name: "Exercicio 3" },
+    { key: "1", name: "Exercicio 1" },
+    { key: "2", name: "Exercicio 2" },
+    { key: "3", name: "Exercicio 3" },
 ];
 
 export default function Exercises() {
-  function Exercise({ key, name }: ExerciseItem) {
-    return (
-      <View style={[styles.page, styles.container]} key={key}>
-        <Text>{name}</Text>
-      </View>
-    );
-  }
+    function Exercise({ key, name }: ExerciseItem) {
+        return (
+            <View style={[styles.page, { height: "100%", width: "100%" }]} key={key}>
+                <View style={{ height: 200, width: 150, backgroundColor: "lightgray" }}>
+                    <Text>{name}</Text>
+                    <View style={{ flexDirection: "row", justifyContent: "space-between"}}>
+                        <Text>Séries</Text>
+                        <Text>3</Text>
+                    </View>
+                    <View style={{ flexDirection: "row", justifyContent: "space-between"}}>
+                        <Text>Repetições</Text>
+                        <Text>8</Text>
+                    </View>
+                    <View style={{ flexDirection: "row", justifyContent: "space-between"}}>
+                        <Text>Carga</Text>
+                        <Text>20</Text>
+                    </View>
+                </View>
+            </View>
+        );
+    }
 
-  return (
-    <View style={styles.container}>
-      <PagerView style={styles.container} initialPage={0}>
-        {ExerciseList.map((item) => (
-          <Exercise key={item.key} name={item.name} />
-        ))}
-      </PagerView>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <PagerView style={styles.container} initialPage={0}>
+                {ExerciseList.map((item) => (
+                    <Exercise key={item.key} name={item.name} />
+                ))}
+            </PagerView>
+        </View>
+    );
 }
