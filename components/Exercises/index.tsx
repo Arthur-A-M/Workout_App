5fd8f71abceb7c73010f4bb7d20af4ba0c5c6a75
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-17 17:22:23 -0300

    Structure Exercises
    Folder Exercises now has a file just to store the styles.

--- components/Exercises/index.tsx
+++ components/Exercises/index.tsx
@@ -1,28 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import PagerView from 'react-native-pager-view';
 
-export default function Exercises() {
-  return (
-    <View style={styles.container}>
-      <PagerView style={styles.container} initialPage={0}>
-        <View style={styles.page} key="1">
-          <Text>Exercise 1</Text>
-        </View>
-        <View style={styles.page} key="2">
-          <Text>Exercise 2</Text>
-        </View>
-      </PagerView>
-    </View>
-  );
-}
+import { styles } from './styles';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-  },
-  page: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+export default function Exercises() {
+    return (
+        <View style={styles.container}>
+            <PagerView style={styles.container} initialPage={0}>
+                <View style={styles.page} key="1">
+                    <Text>Exercise 1</Text>
+                </View>
+                <View style={styles.page} key="2">
+                    <Text>Exercise 2</Text>
+                </View>
+            </PagerView>
+        </View>
+    );
+}
---
+++ components/Exercises/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  page: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
