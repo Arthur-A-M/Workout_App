5870ba7d1f3d283b996a8c3b9d5a8229103390eb
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-17 17:44:53 -0300

    Update index.tsx
    The list of exercises is now generated by mapping an array.

--- components/Exercises/index.tsx
+++ components/Exercises/index.tsx
@@ -4,7 +4,7 @@ import PagerView from 'react-native-pager-view';
 import { styles } from './styles';
 
 export default function Exercises() {
-    function Exercise({key, name}: {key: string, name: string}) {
+    function Exercise({ key, name }: { key: string, name: string }) {
         return (
             <View style={[styles.page, styles.container]} key={key}>
                 <Text>{name} </Text>
@@ -12,12 +12,16 @@ export default function Exercises() {
         );
     }
 
+    const ExerciseList = [
+        { key: "1", name: "Exercicio 1" },
+        { key: "2", name: "Exercicio 2" },
+        { key: "3", name: "Exercicio 3" },
+    ];
+
     return (
         <View style={styles.container}>
             <PagerView style={styles.container} initialPage={0}>
-                <Exercise key="1" name="Exercicio 1" />
-                <Exercise key="2" name="Exercicio 2" />
-                <Exercise key="3" name="Exercicio 3" />
+                {ExerciseList.map((item) => <Exercise key={item.key} name={item.name} />)}
             </PagerView>
         </View>
     );
