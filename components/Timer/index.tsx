81d541ead2b792245ff49d227b7fab97d29f0808
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-17 19:45:53 -0300

    Change timer name and display in mm:ss format
    Created a function that converts the time in raw integer of seconds to mm:ss. Timer displays this value instead of the seconds. Also it is no longer called Timer, it is Descanso.

--- components/Timer/index.tsx
+++ components/Timer/index.tsx
@@ -2,36 +2,42 @@ import { useEffect, useState } from "react";
 import { Pressable, Text, View } from "react-native";
 
 export default function Timer() {
-  const [running, setRunning] = useState(false);
-  const [time, setTime] = useState(0);
+    const [running, setRunning] = useState(false);
+    const [time, setTime] = useState(0);
 
-  useEffect(() => {
-    if (running) {
-      const intervalId = setInterval(() => setTime((prevTime) => prevTime + 1), 1000);
-      return () => {
-        clearInterval(intervalId);
-        setTime(0);
-      };
-    }
-  }, [running]);
+    useEffect(() => {
+        if (running) {
+            const intervalId = setInterval(() => setTime((prevTime) => prevTime + 1), 1000);
+            return () => {
+                clearInterval(intervalId);
+                setTime(0);
+            };
+        }
+    }, [running]);
 
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "white" }}>
-      <Pressable
-        style={{ 
-            height: 100, 
-            width: 100, 
-            justifyContent: "center", 
-            alignItems: "center",
-            borderRadius: 50,
-            borderWidth: 2,
-            borderColor: "black",
-            backgroundColor: running ? "gray" : "lightgray" 
-        }}
-        onPress={() => setRunning(!running)}
-      >
-        {running ? <Text>{time} seconds</Text> : <Text>Timer</Text>}
-      </Pressable>
-    </View>
-  );
+    const formatTime = (time: number) => {
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+        return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    };
+
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "white" }}>
+            <Pressable
+                style={{
+                    height: 100,
+                    width: 100,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius: 50,
+                    borderWidth: 2,
+                    borderColor: "black",
+                    backgroundColor: running ? "gray" : "lightgray"
+                }}
+                onPress={() => setRunning(!running)}
+            >
+                {running ? <Text>{formatTime(time)}</Text> : <Text>Descansar</Text>}
+            </Pressable>
+        </View>
+    );
 }
