1c438f5ba799491b4d615f6cad34f03e4fadf68b
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-17 17:46:59 -0300

    Update index.tsx
    The list of exercises is strongly typed and now it shares a type variable with Exercise

--- components/Exercises/index.tsx
+++ components/Exercises/index.tsx
@@ -3,26 +3,33 @@ import PagerView from 'react-native-pager-view';
 
 import { styles } from './styles';
 
+type ExerciseItem = {
+  key: string;
+  name: string;
+};
+
+const ExerciseList: ExerciseItem[] = [
+  { key: "1", name: "Exercicio 1" },
+  { key: "2", name: "Exercicio 2" },
+  { key: "3", name: "Exercicio 3" },
+];
+
 export default function Exercises() {
-    function Exercise({ key, name }: { key: string, name: string }) {
-        return (
-            <View style={[styles.page, styles.container]} key={key}>
-                <Text>{name} </Text>
-            </View>
-        );
-    }
-
-    const ExerciseList = [
-        { key: "1", name: "Exercicio 1" },
-        { key: "2", name: "Exercicio 2" },
-        { key: "3", name: "Exercicio 3" },
-    ];
-
+  function Exercise({ key, name }: ExerciseItem) {
     return (
-        <View style={styles.container}>
-            <PagerView style={styles.container} initialPage={0}>
-                {ExerciseList.map((item) => <Exercise key={item.key} name={item.name} />)}
-            </PagerView>
-        </View>
+      <View style={[styles.page, styles.container]} key={key}>
+        <Text>{name}</Text>
+      </View>
     );
+  }
+
+  return (
+    <View style={styles.container}>
+      <PagerView style={styles.container} initialPage={0}>
+        {ExerciseList.map((item) => (
+          <Exercise key={item.key} name={item.name} />
+        ))}
+      </PagerView>
+    </View>
+  );
 }
