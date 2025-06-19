05d3c1e4d4a9b47875eb4adea0cdaa7851015bbb
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-19 10:28:49 -0300

    Access both series from the Home
    User can select both series A and B from the home screen.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,37 @@
 import Exercises from "@/components/Exercises";
 import Timer from "@/components/Timer";
-import { View } from "react-native";
+import { useState } from "react";
+import { Pressable, Text, View } from "react-native";
 
 export default function HomeScreen() {
-  return (
-    <View style={{ flex: 1, flexDirection: "column" }}>
-      <Exercises serie='A' />
-      <Timer />
-    </View>
-  );
+
+  const [serie, setSerie] = useState<'A' | 'B' | null>(null);
+
+  function chooseSerie(newSerie: 'A' | 'B') {
+    if (newSerie === serie) {
+      return;
+    }
+    setSerie(newSerie);
+  }
+
+  if (serie === null) {
+    return (
+      <View style={{ flex: 1, flexDirection: "column", justifyContent: "center", alignItems: "center", backgroundColor: "white" }}>
+        <Pressable onPress={() => chooseSerie('A')} style={{ marginBottom: 10 }}>
+          <Text>Exercícios A</Text>
+        </Pressable>
+        <Pressable onPress={() => chooseSerie('B')}>
+          <Text>Exercícios B</Text>
+        </Pressable>
+      </View>
+    );
+  } else {
+    return (
+      <View style={{ flex: 1, flexDirection: "column" }}>
+        <Exercises serie={serie} />
+        <Timer />
+      </View>
+    );
+  }
 }
 
