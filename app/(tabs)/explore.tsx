08ee93c075e9dd6acb9499332e37e970df879437
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-23 11:44:13 -0300

    Add functional local storage
    The app now has functions for storing and reading data locally.

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,9 +1,49 @@
-import { View } from "react-native";
+import { Pressable, Text, View } from "react-native";
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import type { ExerciseItem, SeriesNames } from "@/constants/Data";
+import { listOfSeries } from "@/constants/Data";
 
 export default function TabTwoScreen() {
+  const storeData = async (value: { name: SeriesNames, list: ExerciseItem[] }[] | null) => {
+    if (value === null) {
+      console.error("storeData: value is null");
+      return;
+    }
+
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem('userdata', jsonValue);
+    } catch (error) {
+      console.error("storeData:", error);
+    }
+  };
+
+  const getData = async (key: string) => {
+    try {
+      const jsonValue = await AsyncStorage.getItem(key);
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+      // error reading value
+    }
+  };
+
+  async function checkForSeries() {
+    const data = await getData('userdata');
+    console.log(data);
+    if (data === null) {
+      storeData(listOfSeries);
+    } else {
+      return;
+    }
+  }
+
   return (
-    <View style={{ flex: 1, flexDirection: "column" }}>
-      
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", flexDirection: "column", backgroundColor: "white" }}>
+      <Pressable onPress={checkForSeries}>
+        <Text>Check for series</Text>
+      </Pressable>
     </View>
   );
 }
