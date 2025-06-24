7a063432b5b6725095724435bb3a1d2aa8fa9083
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-24 11:54:59 -0300

    Retrieve data from memory
    Data can be retrieved from memory when accessing HomeScreen.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,12 +2,29 @@ import Exercises from "@/components/Exercises";
 import SerieSeletor from "@/components/SerieSeletor";
 import Timer from "@/components/Timer";
 import { SeriesNames } from "@/constants/Data";
-import { useState } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from "react";
 import { View } from "react-native";
 
 export default function HomeScreen() {
-
   const [serie, setSerie]: [SeriesNames | null, React.Dispatch<React.SetStateAction<SeriesNames | null>>] = useState<SeriesNames | null>(null);
+
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('userdata');
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+      // error reading value
+    }
+  };
+
+  useEffect(() => {
+    getData().then(data => {
+      if (data) {
+        console.log('data', data);
+      }
+    });
+  }, []);
 
   if (serie === null) {
     return (
