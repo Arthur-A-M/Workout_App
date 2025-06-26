a9ab8f70e77ac8404c88ab7660bc4bd48f9c93a8
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-26 17:30:13 -0300

    Add mock button for adding series

--- components/EditSeries/index.tsx
+++ components/EditSeries/index.tsx
@@ -1,14 +1,17 @@
 import { listOfSeriesType } from "@/constants/Data";
 import { ReactElement } from "react";
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 import { styles } from "./styles";
 
-export default function EditSeries({listOfSeries}: {listOfSeries: listOfSeriesType}): ReactElement {
+export default function EditSeries({ listOfSeries }: { listOfSeries: listOfSeriesType }): ReactElement {
     return (
         <View style={styles.container}>
             <Text>Editar Série</Text>
             {listOfSeries.map(serie => <Text key={serie.name}>{serie.name}</Text>)}
+            <Pressable
+                onPress={() => console.log('Adicionar')}
+            ><Text>Adicionar</Text></Pressable>
         </View>
     );
 }
