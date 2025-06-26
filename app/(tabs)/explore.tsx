273ff8d07c5a6001be234aff1d605a3eac949a7e
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-26 17:11:27 -0300

    Explore page now loads stored data

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,9 +1,29 @@
 import Loading from "@/components/Loading";
-import { ReactElement } from "react";
+import { listOfSeriesType, SeriesNames } from "@/constants/Data";
+import { LoadData } from "@/Functions";
+import { ReactElement, useEffect, useState } from "react";
+import { Text, View } from "react-native";
 
 export default function TabTwoScreen(): ReactElement {
-  return (
-    <Loading />
-  );
+  const [listOfSeries, setListOfSeries]: [listOfSeriesType | null, React.Dispatch<React.SetStateAction<listOfSeriesType | null>>] = useState<listOfSeriesType | null>(null);
+  const [serie, setSerie]: [SeriesNames | null, React.Dispatch<React.SetStateAction<SeriesNames | null>>] = useState<SeriesNames | null>(null);
+
+  useEffect(() => {
+    LoadData(setListOfSeries);
+    console.log(listOfSeries);
+  }, []);
+
+  if (listOfSeries === null) {
+    return (
+      <Loading />
+    );
+  }
+  else {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center", flexDirection: "column", backgroundColor: "white" }}>
+        <Text>Explore</Text>
+      </View>
+    );
+  }
 }
 
