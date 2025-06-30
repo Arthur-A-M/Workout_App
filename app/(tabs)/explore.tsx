f7e76ef20dd8686f4b18c866034e62c4ac66a958
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-30 14:03:26 -0300

    Make the new screen an empty component
    The component will not be really empty it will simply have one mock-up text.

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -3,6 +3,7 @@ import Loading from "@/components/Loading";
 import { listOfSeriesType, SeriesNames } from "@/constants/Data";
 import { LoadData } from "@/Functions";
 import { ReactElement, useEffect, useState } from "react";
+import { Text, View } from "react-native";
 
 export default function TabTwoScreen(): ReactElement {
   const [listOfSeries, setListOfSeries]: [listOfSeriesType | null, React.Dispatch<React.SetStateAction<listOfSeriesType | null>>] = useState<listOfSeriesType | null>(null);
@@ -29,7 +30,9 @@ export default function TabTwoScreen(): ReactElement {
   }
   else {
     return (
-      <Loading />
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center", flexDirection: "column", backgroundColor: "white" }}>
+        <Text>Série {serie}</Text>
+      </View>
     );
   }
 }
