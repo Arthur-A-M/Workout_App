0d27f959e75cd766404194128a068038fd52dc9e
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-06-30 14:26:34 -0300

    Clone Exercises
    ExerciseEditor was getting to similar to exercise editor so I decided to just copy and paste the whole folder, safe for name changes.

--- components/ExerciseEditor/index.tsx
+++ components/ExerciseEditor/index.tsx
@@ -1,24 +1,66 @@
-import { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, Text, View } from 'react-native';
+import PagerView from 'react-native-pager-view';
 
 import { ExerciseItem, SeriesNames, listOfSeriesType } from '@/constants/Data';
-
 import { styles } from './styles';
 
-export default function ExerciseEditor({ serie, listOfSeries }: { serie: SeriesNames, listOfSeries: listOfSeriesType }) {
-    function ReturnSerie(serie: SeriesNames): ExerciseItem[] {
+export default function ExerciseEditor({serie = 'A', setSerie, listOfSeries}: {serie: SeriesNames | null | undefined, setSerie: React.Dispatch<React.SetStateAction<SeriesNames | null>>, listOfSeries: listOfSeriesType}) {
+
+    function ReturnSerie(serie: SeriesNames | null): ExerciseItem[] {
         return listOfSeries.find(s => s.name === serie)?.list || listOfSeries[0].list;
     }
 
-    const [seriesList, setSeriesList]: [ExerciseItem[], React.Dispatch<React.SetStateAction<ExerciseItem[]>>] = useState<ExerciseItem[]>(ReturnSerie(serie) );
+    const seriesList: ExerciseItem[] = ReturnSerie(serie);
 
-    useEffect(() => {
-        console.log('seriesList', seriesList);
-    }, [serie]);
-    
+    function Exercise({
+        key,
+        name,
+        series,
+        repetitions,
+        weight
+    }: ExerciseItem) {
+        const [done, setDone] = useState(false);
+
+        return (
+            <View style={styles.page} key={key}>
+                <View style={styles.box}>
+                    <Text>{name}</Text>
+                    <View style={styles.boxRow}>
+                        <Text>Séries</Text>
+                        <Text>{series}</Text>
+                    </View>
+                    <View style={styles.boxRow}>
+                        <Text>Repetições</Text>
+                        <Text>{repetitions}</Text>
+                    </View>
+                    <View style={styles.boxRow}>
+                        <Text>Carga</Text>
+                        <Text>{weight}</Text>
+                    </View>
+                    <Pressable style={styles.boxRow} onPress={() => setDone(!done)}>
+                        <Text>Feito?</Text>
+                        <Text>{done ? 'Sim' : 'Nao'}</Text>
+                    </Pressable>
+                </View>
+            </View>
+        );
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Série {serie}</Text>
+            <Pressable style={styles.backButton} onPress={() => setSerie(null)}><Text>⬅️</Text></Pressable>
+            <PagerView style={styles.container} initialPage={0}>
+                {seriesList.map((item) => (
+                    <Exercise
+                        key={item.key}
+                        name={item.name}
+                        series={item.series}
+                        repetitions={item.repetitions}
+                        weight={item.weight}
+                    />
+                ))}
+            </PagerView>
         </View>
     );
 }
--- components/ExerciseEditor/styles.ts
+++ components/ExerciseEditor/styles.ts
@@ -1,11 +1,30 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        backgroundColor: "white"
-    }
+  container: {
+    height: "80%",
+    width: "100%",
+    backgroundColor: 'white',
+  },
+  backButton: {
+    position: "absolute",
+    top: 50,
+    left: 50,
+    zIndex: 1
+  },
+  page: {
+    height: "100%",
+    width: "100%",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    height: 200,
+    width: 150,
+    backgroundColor: "lightgray"
+  },
+  boxRow: {
+     flexDirection: "row", 
+     justifyContent: "space-between"
+  }
 });
