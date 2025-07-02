ee8b971ebc7f66c029423475fefc7242151f1686
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-07-02 15:56:08 -0300

    Remove unnecessary state handling

--- components/ExerciseEditor/Exercise/index.tsx
+++ components/ExerciseEditor/Exercise/index.tsx
@@ -1,14 +1,9 @@
-import { useState } from 'react';
 import { Text, TextInput, View } from 'react-native';
 
 import { ExerciseItem } from '@/constants/Data';
 import { styles } from './styles';
 
 export default function Exercise({ index, seriesList, setSeriesList }: { index: number, seriesList: ExerciseItem[], setSeriesList: React.Dispatch<React.SetStateAction<ExerciseItem[]>> }) {
-    const [series, setSeries] = useState<string>(seriesList[index].series);
-    const [repetitions, setRepetitions] = useState<string>(seriesList[index].repetitions);
-    const [weight, setWeight] = useState<string>(seriesList[index].weight);
-
     function handleChange(name: keyof ExerciseItem, text: string) {
         setSeriesList(seriesList.map((item, i) => i === index ? { ...item, [name]: text } : item))
     }
