646c317b16ce730465989b2bac9668aeeb22cd19
Arthur <71463136+Arthur-A-M@users.noreply.github.com>
2025-07-02 15:54:10 -0300

    After bug fix, make components Textinputs
    TextInputs were droped before in preference for buttons because of a bug that was causing keyboard to fall every time an input was given. With the component now separate this bug is no longer present and neither the need for buttons.

--- components/ExerciseEditor/Exercise/index.tsx
+++ components/ExerciseEditor/Exercise/index.tsx
@@ -1,46 +1,16 @@
 import { useState } from 'react';
-import { Pressable, Text, TextInput, View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
 import { ExerciseItem } from '@/constants/Data';
 import { styles } from './styles';
 
 export default function Exercise({ index, seriesList, setSeriesList }: { index: number, seriesList: ExerciseItem[], setSeriesList: React.Dispatch<React.SetStateAction<ExerciseItem[]>> }) {
-    const [series, setSeries] = useState<number>(Number(seriesList[index].series));
-    const [repetitions, setRepetitions] = useState<number>(Number(seriesList[index].repetitions));
-    const [weight, setWeight] = useState<number>(Number(seriesList[index].weight));
+    const [series, setSeries] = useState<string>(seriesList[index].series);
+    const [repetitions, setRepetitions] = useState<string>(seriesList[index].repetitions);
+    const [weight, setWeight] = useState<string>(seriesList[index].weight);
 
     function handleChange(name: keyof ExerciseItem, text: string) {
         setSeriesList(seriesList.map((item, i) => i === index ? { ...item, [name]: text } : item))
-    }
-
-    function handleSeriesIncrement() {
-        setSeries(series + 1);
-        handleChange('series', (series + 1).toString());
-    }
-
-    function handleSeriesDecrement() {
-        setSeries(series - 1);
-        handleChange('series', (series - 1).toString());
-    }
-
-    function handleRepetitionsIncrement() {
-        setRepetitions(repetitions + 1);
-        handleChange('repetitions', (repetitions + 1).toString());
-    }
-
-    function handleRepetitionsDecrement() {
-        setRepetitions(repetitions - 1);
-        handleChange('repetitions', (repetitions - 1).toString());
-    }
-
-    function handleWeightIncrement() {
-        setWeight(weight + 0.5);
-        handleChange('weight', (weight + 0.5).toString());
-    }
-
-    function handleWeightDecrement() {
-        setWeight(weight - 0.5);
-        handleChange('weight', (weight - 0.5).toString());
     }
 
     return (
@@ -52,33 +22,27 @@ export default function Exercise({ index, seriesList, setSeriesList }: { index: 
                 />
                 <View style={styles.boxRow}>
                     <Text>Séries</Text>
-                    <Pressable onPress={handleSeriesDecrement}>
-                        <Text>-</Text>
-                    </Pressable>
-                    <Text>{series}</Text>
-                    <Pressable onPress={handleSeriesIncrement}>
-                        <Text>+</Text>
-                    </Pressable>
+                    <TextInput
+                        value={seriesList[index].series}
+                        onChangeText={(text) => handleChange('series', text)}
+                        keyboardType="number-pad"
+                    />
                 </View>
                 <View style={styles.boxRow}>
                     <Text>Repetições</Text>
-                    <Pressable onPress={handleRepetitionsDecrement}>
-                        <Text>-</Text>
-                    </Pressable>
-                    <Text>{repetitions}</Text>
-                    <Pressable onPress={handleRepetitionsIncrement}>
-                        <Text>+</Text>
-                    </Pressable>
+                    <TextInput
+                        value={seriesList[index].repetitions}
+                        onChangeText={(text) => handleChange('repetitions', text)}
+                        keyboardType="number-pad"
+                    />
                 </View>
                 <View style={styles.boxRow}>
                     <Text>Carga</Text>
-                    <Pressable onPress={handleWeightDecrement}>
-                        <Text>-</Text>
-                    </Pressable>
-                    <Text>{weight.toFixed(1)}</Text>
-                    <Pressable onPress={handleWeightIncrement}>
-                        <Text>+</Text>
-                    </Pressable>
+                    <TextInput
+                        value={seriesList[index].weight}
+                        onChangeText={(text) => handleChange('weight', text)}
+                        keyboardType="number-pad"
+                    />
                 </View>
             </View>
         </View>
